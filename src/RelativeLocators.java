5ec0f5a27f636b66150de7884065443333270094
gsesek <destined19876@gmail.com>
2022-12-12 10:12:42 +0100

    12/12/2022 - Added example for relative locators (to left of and to right of)

--- src/RelativeLocators.java
+++ src/RelativeLocators.java
@@ -16,10 +16,17 @@ public class RelativeLocators {
 //        above() - relative locator
         WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
         System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
-
 //        below() - relative locator
-        WebElement dateofBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+        WebElement dateofBirth = driver.findElement(By.xpath("//label[@for='dateofBirth']"));
         driver.findElement(with(By.tagName("input")).below(dateofBirth)).sendKeys("17/03/1987");
+//        to Left of - relatice locator
+        WebElement iceCream = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+        driver.findElement(with(By.tagName("input")).toLeftOf(iceCream)).click();
+//        Getting the label of first radio button
+//        to Right of - relative locator
+        WebElement rButton = driver.findElement(By.id("inlineRadio1"));
+        System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(rButton)).getText());
+
 
     }
 }
